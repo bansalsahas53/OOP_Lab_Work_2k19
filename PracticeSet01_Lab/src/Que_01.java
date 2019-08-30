2eb750657f937438d70ac4a9fdfb0e3ba29c7227
bansalsahas53 <sahas.bansal_cs18@gla.ac.in>
2019-08-30 22:22:33 +0530

    First que. done

---
+++ PracticeSet01_Lab/src/Que_01.java
@@ -0,0 +1,30 @@
+
+package PracticeSet1;
+/*
+
+    @ Author: Sahas Bansal
+
+    @ Version: 12.0.2
+
+ */
+
+import java.util.Scanner;
+
+public class Que_01 {
+    public static void main(String[] args) {
+
+        String KWM = "Welcome, ";
+
+        Scanner scanner = new Scanner(System.in);
+
+        String shizuka = scanner.next();
+
+
+        System.out.println(KWM + "" + shizuka + "!");
+
+
+        scanner.close();
+
+    }
+
+}
